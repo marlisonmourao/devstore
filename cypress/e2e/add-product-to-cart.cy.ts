629be473a470b94db9b693de18e531f5818cf592
marlisonmourao <bentesmourao@gmail.com>
2023-12-10 20:46:05 -0400

    refactor: using new comand to search product

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -23,7 +23,7 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the card', () => {
-    cy.get('input[name=q').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.get('a[href^="/product"]').first().click()
     cy.url().should('include', '/product')
